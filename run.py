21321d334d0a309db1108f63f62acf283a6cd572
Matthew Sebolt <matt@sebolt.us>
2021-06-22 13:43:46 -0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import os, socket, json, types, shutil#, six
+from datetime import datetime
+
+PATH = os.path.abspath(os.path.dirname(__file__))
+REL = os.getcwd()
+
+# public
+async def index(request):
+    return web.FileResponse(PATH + '/index.htm')
+
+# server
+import asyncio, aiohttp
+from aiohttp import web
+from concurrent.futures import ProcessPoolExecutor
+
+def run(part, *args):
+    loop = asyncio.new_event_loop()
+
+    try:
+        full = part(*args)
+        asyncio.set_event_loop(loop)
+        loop.create_task(full)
+        loop.run_forever()
+    finally:
+        loop.close()
+
+async def site(port):
+    app = web.Application(client_max_size=10000000)
+
+    app.router.add_static('/img', 'img')
+    app.add_routes([web.get('/', index)])
+
+    runner = web.AppRunner(app)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    await runner.setup()
+
+    site = web.TCPSite(runner, 'localhost', 5000+port)
+    await site.start()
+
+try:
+    loop = asyncio.get_event_loop()
+    executor = ProcessPoolExecutor(max_workers=14)
+    loop.run_in_executor(executor, run, site, 1) #from haproxy
+
+    loop.run_forever()
+except:
+    pass
+finally:
+    for runner in runners:
+        loop.run_until_complete(runner.cleanup())
